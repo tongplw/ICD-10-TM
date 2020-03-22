87ac592d1f6845edba854c0c164c2f62e00bd33f
tongplw <bangkok.tong@gmail.com>
2020-03-22 19:21:17 +0700

    add a target

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ICD-10-TM
 I need ICD data, but downloading is not provided, so I scraped :D
 
-#### You can access the scraped website in 
-http://203.157.232.109/hdc_report/frontend/web/index.php?r=cicd10tm%2Findex&page=1
+### Target websites 
+1. http://203.157.232.109/hdc_report/frontend/web/index.php?r=cicd10tm%2Findex&page=1
+2. http://61.7.219.187/datacheck/frontend/web/index.php?r=icd10%2Findex&page=1
--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import lxml.html as lh
 import pandas as pd
 import threading
 
-url = 'http://203.157.232.109/hdc_report/frontend/web/index.php?r=cicd10tm%2Findex&page='
-data = []
+# url = 'http://203.157.232.109/hdc_report/frontend/web/index.php?r=cicd10tm%2Findex&page=1'
+url = 'http://61.7.219.187/datacheck/frontend/web/index.php?r=icd10%2Findex&page='
+
 
 START_PAGE = 1
-STOP_PAGE = 2050
-WINDOW = 100
+STOP_PAGE = 500 #2137
+WINDOW = 50
 
 def scrape(url):
     global data
 
     r = requests.get(url)
-    doc = lh.fromstring(r.content)
+    doc = lh.fromstring(r.content.decode('UTF-8'))
     tr_elements = doc.xpath('//tr')
 
     for element in tr_elements[1:]:
@@ -26,6 +27,7 @@ def func(start, stop):
 
         if i%10 == 0: print(i)
 
+data = []
 t = []
 for i in range(START_PAGE, STOP_PAGE, WINDOW):
     x = threading.Thread(target=func, args=(i, i+WINDOW))
@@ -36,5 +38,4 @@ for i in t:
     i.join()
 
 df = pd.DataFrame(data)
-df.columns = ['ลำดับ', 'Diagcode', 'Diagename', 'Diagtname']
-df.to_csv('icd.csv', encoding='utf-8-sig', index=False)
+df.to_csv('output.csv', encoding='utf-8-sig', index=False)
